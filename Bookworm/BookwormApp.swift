52401099c76197e569593bd69c2cc39449b8ebeb
Uriel Ortega <urielortega2509@gmail.com>
2023-05-30 12:45:15 -0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Uriel Ortega on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
